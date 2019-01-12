fdbabeea70cc4faad71232f4c71667cae520b215
huhu <bianlongting@dianwoba.com>
2019-01-12 17:16:23 +0800

    add image gray processing

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ##### 滑块验证码以及点触验证码破解
 
 ```
-qd/ 前端网滑块验证码
+qianduanwang/ 前端网滑块验证码
 src/sms  人工打码
 src/dianchu  同盾点触验证码
 src/index   顺丰滑块验证码
---
+++ binaryzation/index.js
@@ -0,0 +1,40 @@
+const acedia = require("acedia");
+const path = require("path");
+const { createCanvas, Image } = require("canvas");
+
+const width = 320,
+  height = 180;
+
+const canvas = createCanvas(width, height);
+const ctx = canvas.getContext("2d");
+const img = new Image();
+img.src = path.resolve(__dirname, "./1.png");
+ctx.drawImage(img, 0, 0, width, height);
+
+const imgData = ctx.getImageData(0, 0, width, height);
+const { data } = imgData;
+const index = 255 / 2;
+for (var i = 0; i < data.length; i += 4) {
+  var R = data[i];
+  var G = data[i + 1];
+  var B = data[i + 2];
+  var Alpha = data[i + 3];
+  var sum = (R + G + B) / 3;
+  if (sum > index) {
+    data[i] = 255;
+    data[i + 1] = 255;
+    data[i + 2] = 255;
+    data[i + 3] = Alpha;
+  } else {
+    data[i] = 0;
+    data[i + 1] = 0;
+    data[i + 2] = 0;
+    data[i + 3] = Alpha;
+  }
+}
+ctx.putImageData(imgData, 0, 0);
+
+acedia({
+  data: canvas.toDataURL(),
+  dir: path.resolve(__dirname, "./gray.png")
+});
