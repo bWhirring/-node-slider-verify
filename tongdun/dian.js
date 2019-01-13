2568978a9eaf8910a4a3f6ea73bd40b334b6a9bb
huhu <bianlongting@dianwoba.com>
2019-01-13 15:38:46 +0800

    添加点触识别

---
+++ tongdun/dian.js
@@ -0,0 +1,65 @@
+const puppeteer = require("puppeteer");
+const path = require("path");
+var inquirer = require("inquirer");
+const acedia = require("acedia");
+const test = require("./util");
+
+let page = null;
+
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    timeout: 0
+  });
+  page = (await browser.newPage())[0];
+
+  await page.goto(
+    "https://x.tongdun.cn/onlineExperience/textSelection?source=baidu&plan=%E5%8F%8D%E6%AC%BA%E8%AF%88&unit=%E6%99%BA%E8%83%BD%E9%AA%8C%E8%AF%81&keyword=%E6%99%BA%E8%83%BD%E9%AA%8C%E8%AF%81%E7%A0%81&e_creative=24659987438&e_adposition=cl1&e_keywordid=101045415224&e_keywordid2=101045415224&audience=236369",
+    {
+      waitUntil: "networkidle2"
+    }
+  );
+
+  await page.waitForSelector("#loginBtn");
+
+  await page.click("#loginBtn");
+  const bg = ".td-bg-img";
+  await page.waitForSelector(bg);
+
+  await page.waitFor(1000);
+  const bgElement = await page.$(bg);
+  let { data } = await page.evaluate(() => {
+    const bgImg = document.querySelector(".td-bg-img");
+    return {
+      data: bgImg.toDataURL()
+    };
+  });
+  acedia({
+    data,
+    dir: path.resolve(__dirname, "test.png")
+  });
+  const { x, y } = await bgElement.boundingBox();
+  const { pos } = await inquirer.prompt({
+    name: "pos",
+    message: "请输入点坐标"
+  });
+  const { pic_str } = await test();
+  const allPos = pic_str.split("|"); // '55,83|236,37|126,128'
+
+  const inputNumArr = pos.split(",");
+  let arr = [];
+  inputNumArr.map(v => {
+    arr.push(allPos[v]);
+  });
+  for (let i = 0; i < arr.length; i++) {
+    console.log(x + Number(arr[i].split(",")[0]));
+    console.log(y + Number(arr[i].split(",")[1]));
+    await page.mouse.click(
+      x + Number(arr[i].split(",")[0]),
+      y + Number(arr[i].split(",")[1])
+    );
+    await page.waitFor(2000);
+  }
+}
+
+run();
---
+++ tongdun/util.js
@@ -0,0 +1,38 @@
+var rest = require("@dwd/restler"),
+  fs = require("fs"),
+  path = require("path"),
+  filename = path.resolve(__dirname, "./test.png");
+
+async function getPosition() {
+  return new Promise(resolve => {
+    rest
+      .post("http://upload.chaojiying.net/Upload/Processing.php", {
+        multipart: true,
+        data: {
+          user: "用户",
+          pass: "密码",
+          softid: "软件ID", //软件ID 可在用户中心生成
+          codetype: "9004", //验证码类型 http://www.chaojiying.com/price.html 选择
+          userfile: rest.file(
+            filename,
+            null,
+            fs.statSync(filename).size,
+            null,
+            "image/gif"
+          ) // filename: 抓取回来的码证码文件
+        },
+        headers: {
+          "User-Agent":
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0",
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      })
+      .on("complete", function(data) {
+        console.log(data);
+        resolve(data);
+        console.log("Captcha Encoded.");
+      });
+  });
+}
+
+module.exports = getPosition;
